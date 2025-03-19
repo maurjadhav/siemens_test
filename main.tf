a7be3fc6e11900b277b1ca846aca22ed62123267
mayur jadhav <maur.r.jadhav@gmail.com>
2025-03-19 15:27:22 +0530

    added changes for lambda function

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "lambda_sg" {
 }
 
 # AWS Lambda Function
-resource "aws_lambda_function" "lambda_function" {
+resource "aws_lambda_function" "lambda_fun" {
   function_name = "api_lambda"
   role          = data.aws_iam_role.lambda.arn
   handler       = "lambda_function.lambda_handler"
@@ -74,6 +74,6 @@ resource "aws_lambda_function" "lambda_function" {
 
 # CloudWatch Log Group for Lambda
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
+  name              = "/aws/lambda/${aws_lambda_function.lambda_fun.function_name}"
   retention_in_days = 7
 }
