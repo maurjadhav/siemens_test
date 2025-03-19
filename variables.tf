c48420fe23673db90ebbd1c1af5ee17c6b5144e9
mayur jadhav <maur.r.jadhav@gmail.com>
2025-03-19 13:47:11 +0530

    added first and last name as default value

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "first_name" {
   type        = string
   description = "Your first name"
+  default = "mayur"
 }
 
 variable "last_name" {
   type        = string
   description = "Your last name"
+  default = "jadhav"
 }
