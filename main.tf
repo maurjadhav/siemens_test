37696aeab42c049868024ba74d51b44bf004a58e
mayur jadhav <maur.r.jadhav@gmail.com>
2025-03-19 14:57:36 +0530

    added changes for rout table

--- main.tf
+++ main.tf
@@ -28,6 +28,10 @@ resource "aws_route_table" "private_rt" {
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
+
+  lifecycle {
+    prevent_destroy = true        # Prevents accidental deletion
+  }
 }
 
 # Security Group for Lambda
