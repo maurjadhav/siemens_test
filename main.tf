8d379436365ea1ea29920f5237e6795a88ba3eae
mayur jadhav <maur.r.jadhav@gmail.com>
2025-03-19 16:36:16 +0530

    added changes for lambda function

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "lambda_sg" {
 
 # AWS Lambda Function
 resource "aws_lambda_function" "lambda_function" {
-  function_name = "mayur_fun134679"
+  function_name = "mayur_fun_new"
   role          = data.aws_iam_role.lambda.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
